cb8215d7b52ff8cdae20b1d470453a3dd1ec6bc5
kishorekumarsigroha <kmasigroha@gmail.com>
2025-11-04 14:41:58 +0530

    first commit

---
+++ kishore.py
@@ -0,0 +1,20 @@
+a,b=10.2,2
+
+print(a)
+print(b)
+print(a*b)
+print(a/b)
+
+list=[1,3,4,5,6,"krishna"]
+print(list[3])
+print(list[-1])
+
+colors = ("red", "green", "blue")
+print(colors[0])     # 'red'
+print(colors[1])
+
+
+fruits = ["apple", "banana", "cherry"]
+fruits[1] = "blueberry"   # You can modify lists
+fruits.append("mango")
+print(fruits)
